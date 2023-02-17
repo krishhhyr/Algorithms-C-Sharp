34aec68c283337748ce558edfe98551344a267ec
Krishna Rajamannar <17rajamannark@rcmail.org.uk>
2023-02-17 12:55:43 +0000

    Added Linear Search

---
+++ AlgorithmsCSharp/LinearSearch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinearSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
