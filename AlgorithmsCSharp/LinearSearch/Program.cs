241107d494f7092702ac1d2b8133df33a4395fd1
Krishna R <rajamannar@hotmail.com>
2023-04-23 12:20:20 +0100

    alt version of linear search from craigndave pseudocode

--- AlgorithmsCSharp/LinearSearch/Program.cs
+++ AlgorithmsCSharp/LinearSearch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LinearSearch
 {
@@ -6,30 +7,60 @@ namespace LinearSearch
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 4, 6, 345, 123, 34, 12, 4, 35 };
-            Console.WriteLine("Enter a value:");
-            int key = Convert.ToInt32(Console.ReadLine());
-            LinearSearch(array, key);
+            //int[] array = { 1, 4, 6, 345, 123, 34, 12, 4, 35 };
+            //Console.WriteLine("Enter a value:");
+            //int key = Convert.ToInt32(Console.ReadLine());
+            //LinearSearch(array, key);
 
-            void LinearSearch(int[] array, int key) 
+            //void LinearSearch(int[] array, int key) 
+            //{
+            //    bool Found = false;
+            //    for (int i = 0; i < array.Length; i++) 
+            //    {
+            //        if (array[i] == key)
+            //        {
+            //            Found = true;
+            //            break;
+            //        }
+            //    }
+            //    if (Found == true)
+            //    {
+            //        Console.WriteLine("Found in list.");
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("Not found in list.");
+            //    }
+            //}
+
+            Alternative();
+        }
+        static void Alternative() 
+        {
+            int[] dataset = {10, 12, 928, 38, 82, 82, 892, 618, 1, 10  };
+            int item = 618;
+            bool found = false;
+            int position = 0;
+
+            while ((found == false) && (position <= dataset.Length - 1)) 
             {
-                bool Found = false;
-                for (int i = 0; i < array.Length; i++) 
+                if (dataset[position] == item)
                 {
-                    if (array[i] == key)
-                    {
-                        Found = true;
-                        break;
-                    }
-                }
-                if (Found == true)
-                {
-                    Console.WriteLine("Found in list.");
+                    found = true;
                 }
                 else 
                 {
-                    Console.WriteLine("Not found in list.");
+                    position++;
                 }
+            }
+
+            if (found == true)
+            {
+                Console.WriteLine("Item found at " + position);
+            }
+            else 
+            {
+                Console.WriteLine("Item not found in list");
             }
         }
     }
