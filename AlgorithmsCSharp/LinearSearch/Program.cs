1310d5e427d642e651206dfe838cca35a13e913f
Krishna R <rajamannar@hotmail.com>
2023-04-23 12:20:49 +0100

    alt linear search

--- AlgorithmsCSharp/LinearSearch/Program.cs
+++ AlgorithmsCSharp/LinearSearch/Program.cs
@@ -7,33 +7,32 @@ namespace LinearSearch
     {
         static void Main(string[] args)
         {
-            //int[] array = { 1, 4, 6, 345, 123, 34, 12, 4, 35 };
-            //Console.WriteLine("Enter a value:");
-            //int key = Convert.ToInt32(Console.ReadLine());
-            //LinearSearch(array, key);
+            int[] array = { 1, 4, 6, 345, 123, 34, 12, 4, 35 };
+            Console.WriteLine("Enter a value:");
+            int key = Convert.ToInt32(Console.ReadLine());
+            LinearSearch(array, key);
 
-            //void LinearSearch(int[] array, int key) 
-            //{
-            //    bool Found = false;
-            //    for (int i = 0; i < array.Length; i++) 
-            //    {
-            //        if (array[i] == key)
-            //        {
-            //            Found = true;
-            //            break;
-            //        }
-            //    }
-            //    if (Found == true)
-            //    {
-            //        Console.WriteLine("Found in list.");
-            //    }
-            //    else 
-            //    {
-            //        Console.WriteLine("Not found in list.");
-            //    }
-            //}
+            void LinearSearch(int[] array, int key)
+            {
+                bool Found = false;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == key)
+                    {
+                        Found = true;
+                        break;
+                    }
+                }
+                if (Found == true)
+                {
+                    Console.WriteLine("Found in list.");
+                }
+                else
+                {
+                    Console.WriteLine("Not found in list.");
+                }
+            }
 
-            Alternative();
         }
         static void Alternative() 
         {
